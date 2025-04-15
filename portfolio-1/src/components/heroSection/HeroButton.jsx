d6dc21d5141f2280fe03b297daf9eab24f8878a3
Marouane Katfi <mkatfi@c3r1p10.1337.ma>
2025-04-15 20:06:53 +0100

    finito

--- portfolio-1/src/components/heroSection/HeroButton.jsx
+++ portfolio-1/src/components/heroSection/HeroButton.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { FaGithub } from "react-icons/fa";
+import { TbFileCv } from "react-icons/tb";
 
 const HeroButton = () => {
   return (
-    <div className="flex gap-4 gap-10 ml-10">
+    <div className="flex gap-10 ml-10">
       <a
         href="../../../public/cv/Marouane-software.pdf"
         download
       >
-        <button className="px-4 py-2 bg-orange text-black rounded hover:bg-orange text-xl font-bold font-body  border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow
+        <button className="hover:text-white text-center px-4 py-2 bg-orange text-black rounded hover:bg-orange text-xl font-bold font-body  border-cyan border flex items-center gap-1 bg-gradient-to-r  from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow
         ">
           My CV
+          <TbFileCv className='font-bold size-6 text-center  ' />
         </button>
       </a>
 
@@ -18,8 +21,9 @@ const HeroButton = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="px-4 py-2 bg-orange text-black rounded hover:bg-orange text-xl font-bold font-body  border-cyan border flex items-center gap-1 bg-gradient-to-r from-orange to-darkCyan  transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
+        <button className="hover:text-white text-center px-4 py-2 bg-orange text-black rounded hover:bg-orange text-xl font-bold font-body  border-cyan border flex items-center gap-1 bg-gradient-to-r from-orange to-darkCyan  transition-all duration-500 hover:scale-110 hover:border-orange cursor-pointer hover:shadow-cyanShadow">
           My GitHub
+          <FaGithub className='font-bold size-6 text-center  ' />
         </button>
       </a>
     </div>
